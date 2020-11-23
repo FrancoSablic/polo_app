42128a129cbb9056f423e09c4b505000a7841316
FrancoSablic <francosablic@live.com.ar>
2020-11-23 17:51:46 -0300

    prueba con bootstrap

--- polo_app/urls.py
+++ polo_app/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     
     path('', views.index, name='index'),
     path('productos', views.productos, name='productos'),
-    path('detalle_productos', views.detalle_productos, name="detalle_productos"),
-    path('pedir_turnos',views.pedir_turnos, name="pedir_turnos"),
-    path('contacto', views.contacto, name="contacto"),
+    path('detalle_productos', views.detalle_productos, name='detalle_productos'),
+    path('pedir_turnos',views.pedir_turnos, name='pedir_turnos'),
+    path('contacto', views.contacto, name='contacto'),
 ]
 
